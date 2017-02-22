2031ea5b9c08108f6b9a3fd4f5f930a330461e29
Zoltan Andras <zoltan.andras.1884@gmail.com>
2017-02-22 22:45:53 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package YogaFlyTheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yoga-fly-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'yoga-fly-theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'yoga-fly-theme' ), 'yoga-fly-theme', '<a href="https://automattic.com/" rel="designer">ZoLee</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
